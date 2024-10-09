448d7775ac994977ac09455f777849bd5a4d568d
narumeline <emeline.boulic22@gmail.com>
2024-10-09 11:35:36 +0200

    hihi

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.set('view egine','ejs');
+app.set('view engine','ejs');
 
 app.use(express.static('public'));
 
 app.get('/', function(req,res){
-    res.render('index');
+    let data ={
+        prenom: "Bob", 
+        nom : "Marley"
+    }
+    res.render('index', {data});
+
 });
 
 app.use(function(req,res){
