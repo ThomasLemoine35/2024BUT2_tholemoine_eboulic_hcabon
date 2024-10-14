a8080fa705e44ce63dfffd8750abceaabd130dd8
HCabon <hadrien.cabon@gmail.com>
2024-10-14 14:48:56 +0200

    fix de fautes

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const bd = require("./database.js");
 
 async function getUserById (id){
-    sql="SELECT * FRO% utilisateur WHERE id= ?";
+    sql="SELECT * FROM utilisateur WHERE id= ?";
     return new Promise((resolve,reject)=>{
         bdd.query(sql,id,(err,results)=>{
             if (err){
