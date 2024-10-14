d15e09a953f27dc0282eb69f2f40b427dcf2e77b
HCabon <hadrien.cabon@gmail.com>
2024-10-14 14:45:22 +0200

    egffgtr

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const userModel=require("./database.js");
+const userModel=require("./models/user.js");
 
 
 app.set('view engine','ejs');
